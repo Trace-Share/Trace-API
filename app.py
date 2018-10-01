40437f6c5b2fd8c9c11931870d9f586b0e4b398c
peter <422690@mail.muni.cz>
2018-10-01 20:32:53 +0200

    Init app

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_restplus import Resource, Api
+
+
+app = Flask(__name__)
+api = Api(app)
+
+
+@api.route("/hello")
+class HelloWorld(Resource):
+
+    def get(self):
+        return dict(abc=2)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
