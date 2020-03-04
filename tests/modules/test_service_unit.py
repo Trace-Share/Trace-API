c09e34a54c65a9ef7d4ff5f03ad690a3a57fd5da
Tomas Madeja <t.mm.madeja@gmail.com>
2020-03-04 22:42:48 +0100

    quack

--- tests/modules/test_service_unit.py
+++ tests/modules/test_service_unit.py
@@ -158,7 +158,7 @@ def test_unit_normalize_invalid_ip_details(service_unit, file_hydra_1_binary):
             intermediate_nodes=[],
             source_nodes=[]
         ),
-        timestamp=Mapping()
+        tcp_timestamp_mapping=Mapping()
     )
     assert annotated_unit.id_annotated_unit
 
